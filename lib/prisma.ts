614f4f4efbe37762f79205feeea645ceefdf98c8
pizht <chaoxichun@outlook.com>
2026-02-22 13:26:33 +0800

    postgres

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,14 +1,9 @@
 import { PrismaClient } from '@prisma/client';
-import path from 'path';
 
 const globalForPrisma = globalThis as unknown as {
   prisma: PrismaClient | undefined;
 };
 
-const dbPath = path.join(process.cwd(), 'prisma', 'dev.db');
-
-export const prisma = globalForPrisma.prisma ?? new PrismaClient({
-  datasourceUrl: `file:${dbPath}`,
-});
+export const prisma = globalForPrisma.prisma ?? new PrismaClient();
 
 if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma;
